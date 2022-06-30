281be00c5eef24cfc6dd320b2602f24a617ba680
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-06-30 15:44:45 +0200

    Create the class used to send the flag messages

---
+++ KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/utils/KeramixNotificationsUtils.java
@@ -0,0 +1,14 @@
+package com.github.lory24.keramix.core.p47.utils;
+
+import com.github.lory24.keramix.core.p47.checks.AbstractCheck;
+
+@SuppressWarnings("SpellCheckingInspection")
+public class KeramixNotificationsUtils {
+
+    /**
+     * Send the notification to every player that have the permission to read the notifications
+     */
+    public static void sendFlagNotification(AbstractCheck check, int times) {
+
+    }
+}
