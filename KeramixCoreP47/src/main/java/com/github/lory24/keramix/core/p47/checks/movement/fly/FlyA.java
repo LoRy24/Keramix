bbf7cfcebb878a0d71a8fad4c9c98b0739888d76
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-07-01 16:31:58 +0200

    Started working on FlyA check

--- KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/checks/movement/fly/FlyA.java
+++ KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/checks/movement/fly/FlyA.java
@@ -3,8 +3,16 @@ package com.github.lory24.keramix.core.p47.checks.movement.fly;
 import com.github.lory24.keramix.core.p47.checks.AbstractCheck;
 import com.github.lory24.keramix.core.p47.checks.ChecksIdentifiers;
 import com.github.lory24.keramix.core.p47.checks.SubChecker;
+import com.github.lory24.keramix.core.p47.utils.HackedPlayer;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.jetbrains.annotations.NotNull;
 
 public class FlyA extends AbstractCheck {
+
+    // Used to obtain when to start flagging
+    private int totalTimes = 0;
 
     /**
      * Set all the most important features of the check.
@@ -19,7 +27,24 @@ public class FlyA extends AbstractCheck {
      * Process the check
      */
     @Override
-    public void check() {
-        this.fail(); // Fail the check just for testing
+    public void check(@NotNull final HackedPlayer player) {
+        // Locations
+        final Location from = player.getHackedPlayerData().getPlayerLocation().getFromValue();
+        final Location to = player.getHackedPlayerData().getPlayerLocation().getToValue();
+
+        // Fix on player join error
+        if (to == null || from == null) return;
+
+        // Get the entity player
+        final EntityPlayer entityPlayer = ((CraftPlayer) player.getPlayer()).getHandle();
+
+        // asus
+        final boolean clientOnGround = entityPlayer.onGround;
+        final boolean serverOnGround = to.getY() % 0.015625 == 0.0 && from.getY() % 0.015625 == 0.0;
+
+        if (clientOnGround != serverOnGround) {
+            if (++this.totalTimes > 4)
+                this.fail();
+        } else totalTimes = 0;
     }
 }
