718118556a4f85c6cafc63091be1691f2402793b
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-07-01 15:10:16 +0200

    Finish the notification util

--- KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/utils/KeramixNotificationsUtils.java
+++ KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/utils/KeramixNotificationsUtils.java
@@ -1,6 +1,8 @@
 package com.github.lory24.keramix.core.p47.utils;
 
+import com.github.lory24.keramix.core.p47.ConfigValues;
 import com.github.lory24.keramix.core.p47.checks.AbstractCheck;
+import org.bukkit.Bukkit;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class KeramixNotificationsUtils {
@@ -9,6 +11,12 @@ public class KeramixNotificationsUtils {
      * Send the notification to every player that have the permission to read the notifications
      */
     public static void sendFlagNotification(AbstractCheck check, int times) {
-
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            if (player.hasPermission("keramix.flag.see")) {
+                String message = ConfigValues.messages_flagMessage.getParsedStringFromConfig().replace("${player}", player.getName()).replace("${flag-name}",
+                        check.getIdentifier().getIdentifierText()).replace("${flag-times}", String.valueOf(times));
+                player.sendMessage(message);
+            }
+        });
     }
 }
