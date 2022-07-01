fcd501f194fde9752db619708d4f965ee77af100
LoRy24 <51451299+LoRy24@users.noreply.github.com>
2022-07-01 15:09:34 +0200

    Add try and catch

--- KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/Core.java
+++ KeramixCoreP47/src/main/java/com/github/lory24/keramix/core/p47/Core.java
@@ -3,6 +3,8 @@ package com.github.lory24.keramix.core.p47;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+
 @SuppressWarnings("unused")
 public class Core {
 
@@ -10,7 +12,11 @@ public class Core {
      * When the core enables
      */
     public void enable(@NotNull JavaPlugin javaPlugin) {
-        KeramixAntiCheat.INSTANCE.enable(javaPlugin);
+        try {
+            KeramixAntiCheat.INSTANCE.enable(javaPlugin);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
